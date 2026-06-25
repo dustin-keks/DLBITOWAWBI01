659e5f3a33dfd4715d8e68d3dcf35f1fcf0bff35
Dustin Vetter <dustinvetter@icloud.com>
2026-06-25 21:56:39 +0200

    frontend: add Projektliste component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ProjektlisteComponent} from './projekte/projektliste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'projekte', component: ProjektlisteComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+];
---
+++ frontend/src/app/projekte/projektliste.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-projektliste',
+  templateUrl: './projektliste.component.html',
+})
+export class ProjektlisteComponent {}
