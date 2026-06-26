b48a5b0b53f6d3c4b2a0b387266d99f87a04c701
Dustin Vetter <dustinvetter@icloud.com>
2026-06-27 01:20:17 +0200

    frontend: handle HTTP 401 in authentication interceptor

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -1,19 +1,25 @@
 import {HttpInterceptorFn} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
+import {catchError, throwError} from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
+  const router = inject(Router);
 
-  if (token) {
-    const authReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
+  const authReq = token
+    ? req.clone({setHeaders: {'Authorization': `Bearer ${token}`}})
+    : req;
+
+  return next(authReq).pipe(
+    catchError((err) => {
+      if (err.status === 401) {
+        authService.logout();
+        router.navigateByUrl('/login');
       }
-    });
-    return next(authReq);
-  }
-
-  return next(req);
+      return throwError(() => err);
+    })
+  );
 };
